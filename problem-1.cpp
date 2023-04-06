edea23acf88d6c60756caa14be0c44effeb184d7
Georgiosss <59983624+Georgiosss@users.noreply.github.com>
2023-04-06 22:00:59 +0400

    Update problem-1.cpp

--- problem-1.cpp
+++ problem-1.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int & x : arr) cin >> x;
+    sort(arr.begin(), arr.end());
+    for (int x : arr)
+        cout << x << " ";
+    cout << endl;
     return 0;
 }
